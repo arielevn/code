1be1b2dab918deb1b1b57590a7bf26a2fead6d93
arielevn <arielevn@gmail.com>
2024-03-29 15:46:43 +0800

    Update script.js
    change method math.random to Math.random
    
    add parentheses for calling getComputerChoice function

--- odin_assignment js/RockPaperScissors/script.js
+++ odin_assignment js/RockPaperScissors/script.js
@@ -2,4 +2,4 @@ function getComputerChoice (){
     let choice = ["Rock", "Paper", "Scissor"];
     return randomChoice = choice[Math.floor(Math.random()*choice.length)];
 }
-console.log(getComputerChoice)
+console.log(getComputerChoice())
