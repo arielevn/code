ddcc6d03e2ae81ce61b71c2c167f4ad11f3a76f5
arielevn <arielevn@gmail.com>
2024-03-29 15:44:02 +0800

    create js for computer game

---
+++ odin_assignment js/RockPaperScissors/script.js
@@ -0,0 +1,5 @@
+function getComputerChoice (){
+    let choice = ["Rock", "Paper", "Scissor"];
+    return randomChoice = choice[Math.floor(Math.random()*choice.length)];
+}
+console.log(getComputerChoice)
