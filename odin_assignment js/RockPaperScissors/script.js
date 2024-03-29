459e7b67ee492f8e60de0edbb2e23128706b18a6
arielevn <arielevn@gmail.com>
2024-03-29 17:37:46 +0800

    Update script.js
    assigned the result back to computerChoice. not necessary.

--- odin_assignment js/RockPaperScissors/script.js
+++ odin_assignment js/RockPaperScissors/script.js
@@ -1,6 +1,6 @@
 function getComputerChoice (){
     let computerChoice = ["Rock", "Paper", "Scissor"];
-    return computerChoice = computerChoice[Math.floor(Math.random()*computerChoice.length)];
+    return computerChoice [Math.floor(Math.random()*computerChoice.length)];
 }
 
 function playerSelection(){
