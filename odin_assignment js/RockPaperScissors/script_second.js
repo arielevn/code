e744fa1f0a72723202098066641a84e74a11d65d
arielevn <arielevn@gmail.com>
2024-04-01 12:17:26 +0800

    Update script_second.js
    ADD parenthesis in getComputerChoice function

--- odin_assignment js/RockPaperScissors/script_second.js
+++ odin_assignment js/RockPaperScissors/script_second.js
@@ -1,9 +1,9 @@
-function getComputerChoice{
+function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     return choices(Math.floor[Math.random() * choices.length]);
 }
 
-function playround(playerSelection, computerSelection){
+function playround(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 }
