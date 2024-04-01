a202f408711b93b7719ee56fd75b43c5ea9bc101
arielevn <arielevn@gmail.com>
2024-04-01 11:58:33 +0800

    Update script_second.js
    ADD playround function
    GET playerSelection case insensitive
    GET computerSelection case insensitive

--- odin_assignment js/RockPaperScissors/script_second.js
+++ odin_assignment js/RockPaperScissors/script_second.js
@@ -2,3 +2,8 @@ function getComputerChoice{
     let choices = ["rock", "paper", "scissor"]
     return choices(Math.floor[Math.random() * choices.length])
 }
+
+function playround(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+}
