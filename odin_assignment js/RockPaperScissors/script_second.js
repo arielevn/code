b1085e07f73fc423a10007d82afda5e6a99bf38e
arielevn <arielevn@gmail.com>
2024-04-06 17:30:07 +0800

    Update script_second.js
    ADD conditional statement to play the game

--- odin_assignment js/RockPaperScissors/script_second.js
+++ odin_assignment js/RockPaperScissors/script_second.js
@@ -6,4 +6,13 @@ function getComputerChoice() {
 function playround(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+
+    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock"))
+        {return "You Win!" + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + "beats" + computerSelection.charAt(0).toUpperCase + computerSelection.slice(1)} 
+        else if (playerSelection == computerSelection)
+            {return "It's a tie!"}
+        else ()
+            return {"You Lose!"}
 }
