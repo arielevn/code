3aad4fdcbdec785d30e60e366f92655f38d17b66
arielevn <arielevn@gmail.com>
2024-04-07 12:26:47 +0800

    Update script_second.js
    MOVE semicolon in return inside the curly brace
    REMOVE parenthesis in else

--- odin_assignment js/RockPaperScissors/script_second.js
+++ odin_assignment js/RockPaperScissors/script_second.js
@@ -9,11 +9,20 @@ function playround(playerSelection, computerSelection) {
 
     if ((playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock"))
-        {return "You Win!" + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + 
-        "beats" + computerSelection.charAt(0).toUpperCase + computerSelection.slice(1)}; 
-        else if (playerSelection == computerSelection)
-            return "It's a tie!";
-        else ()
-            return "You Lose!" + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + "beats" + playerSelection.charAt(0).toUpperCase + playerSelection.slice(1);
+        (playerSelection == "paper" && computerSelection == "rock")){
+            return "You Win!" + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + 
+        "beats" + computerSelection.charAt(0).toUpperCase + computerSelection.slice(1); 
+    }else if (playerSelection == computerSelection)
+            {return "It's a tie!";
+    }else
+            {return "You Lose!" + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + "beats" + playerSelection.charAt(0).toUpperCase + playerSelection.slice(1);
+        }
+    }
+
+function playgame(){
+    let i = 0;
+    let playRound = i, i++;
+        for playRound == 5;
+            return 
 }
+    
