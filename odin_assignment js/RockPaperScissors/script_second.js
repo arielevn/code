ff36e5180bcb24696db969d05f60eff01dc9ae3e
arielevn <arielevn@gmail.com>
2024-04-10 15:27:32 +0800

    Update script_second.js
    modify indentation inside playRound
    
    remove dot in playGame result variable

--- odin_assignment js/RockPaperScissors/script_second.js
+++ odin_assignment js/RockPaperScissors/script_second.js
@@ -7,15 +7,16 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    if ((playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock")){
-            return "You Win!" + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + 
-        "beats" + computerSelection.charAt(0).toUpperCase + computerSelection.slice(1); 
-    }else if (playerSelection == computerSelection)
-            {return "It's a tie!";
-    }else
-            {return "You Lose!" + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + "beats" + playerSelection.charAt(0).toUpperCase + playerSelection.slice(1);
+    if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")){
+        return "You Win!" + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + 
+            "beats" + computerSelection.charAt(0).toUpperCase + computerSelection.slice(1); 
+    }else if (playerSelection === computerSelection){
+        return "It's a tie!";
+    }else{
+        return "You Lose!" + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + 
+        "beats" + playerSelection.charAt(0).toUpperCase + playerSelection.slice(1);
         }
     }
 
@@ -24,7 +25,7 @@ function playGame(){
         let playerSelection = prompt("Enter your choice: ");
         let computerSelection = getComputerChoice();
 
-        let.result = playRound();
+        let result = playRound();
         console.log(result);
     }
 }
