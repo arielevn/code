2767e957c2d24cdb46d36e7612e28308555660f1
arielevn <arielevn@gmail.com>
2024-04-12 16:30:34 +0800

    Update loop.py
    exercise 3
    > return True if the there are 2 items that are equal
    
    exercise 4
    > return the average of profit from player

--- kaggle_python/python/loop.py
+++ kaggle_python/python/loop.py
@@ -21,3 +21,29 @@ def elementwise_greater_than(L, thresh):
     for x in L:
         result.append(x > thresh)
     return result
+
+# exercise 3
+def menu_is_boring(meals):
+    """Given a list of meals served over some period of time, return True if the
+    same meal has ever been served two days in a row, and False otherwise.
+
+    """
+    for i in range(len(meals)-1):
+        if meals[i] == meals[i+1]:
+            return True
+    return False
+    pass
+
+# exercise 4
+def estimate_average_slot_payout(n_runs):
+    """Run the slot machine n_runs times and return the average net profit per run.
+    Example calls (note that return value is nondeterministic!):
+    >>> estimate_average_slot_payout(1)
+    -1
+    >>> estimate_average_slot_payout(1)
+    0.5
+    """
+    for i in range(n_runs):
+        profit = play_slot_machine()
+        average_profit = sum(profit)/n_runs
+    return average_profit
