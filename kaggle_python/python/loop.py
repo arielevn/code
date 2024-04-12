d01744c80271d5fdc0fff6ca05b45d32ccaec671
arielevn <arielevn@gmail.com>
2024-04-12 12:45:56 +0800

    Create loop.py
    exercise 1
    RETURN True if num can be divided by 7
    
    exercise 2
    append result list if item in L is above thresh

---
+++ kaggle_python/python/loop.py
@@ -0,0 +1,23 @@
+#exercise 1
+def has_lucky_number(nums):
+    """Return whether the given list of numbers is lucky. A lucky list contains
+    at least one number divisible by 7.
+    """
+    for num in nums:
+        if num % 7 == 0:
+            return True
+        else:
+            return False
+
+#exercise 2
+def elementwise_greater_than(L, thresh):
+    """Return a list with the same length as L, where the value at index i is 
+    True if L[i] is greater than thresh, and False otherwise.
+    
+    >>> elementwise_greater_than([1, 2, 3, 4], 2)
+    [False, False, True, True]
+    """
+    result = []
+    for x in L:
+        result.append(x > thresh)
+    return result
