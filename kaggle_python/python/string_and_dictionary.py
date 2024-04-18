0c1b97f613e6b87576a5a5d18d5839a9663d1a6f
arielevn <arielevn@gmail.com>
2024-04-18 20:34:09 +0800

    Update string_and_dictionary.py
    ADD exercise 2 second option from kaggle solution

--- kaggle_python/python/string_and_dictionary.py
+++ kaggle_python/python/string_and_dictionary.py
@@ -28,3 +28,20 @@ def word_search(doc_list, keyword):
             keyword_indices.append(i)
 
     return keyword_indices
+
+#exercise 2 >> second option from kaggle solution
+
+def word_search(doc_list, keyword):
+    # list to hold the indices of matching documents
+    indices = [] 
+    # Iterate through the indices (i) and elements (doc) of documents
+    for i, doc in enumerate(doc_list):
+        # Split the string doc into a list of words (according to whitespace)
+        tokens = doc.split()
+        # Make a transformed list where we 'normalize' each word to facilitate matching.
+        # Periods and commas are removed from the end of each word, and it's set to all lowercase.
+        normalized = [token.rstrip('.,').lower() for token in tokens]
+        # Is there a match? If so, update the list of matching indices.
+        if keyword.lower() in normalized:
+            indices.append(i)
+    return indices
