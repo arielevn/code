9ec501bb07b0540ddaaddaebca0bc20a94c8b61a
arielevn <arielevn@gmail.com>
2024-06-18 00:52:43 +0800

    Update script_second.js
    ADD space in result print
    REMOVE 5 times loop

--- odin_assignment js/RockPaperScissors/script_second.js
+++ odin_assignment js/RockPaperScissors/script_second.js
@@ -14,24 +14,24 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock")){
-        return "You Win!" + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + 
-            "beats" + computerSelection.charAt(0).toUpperCase + computerSelection.slice(1); 
+        return "You Win!" + " " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " " +
+            "beats" + " " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1); 
     }else if (playerSelection === computerSelection){
         return "It's a tie!";
     }else{
-        return "You Lose!" + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + 
-        "beats" + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        return "You Lose!" + " " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + 
+        "beats" + " "  + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
         }
     }
 
-// function playGame(){
-//     for (let i = 0; i < 5; i++){
-//         let playerSelection = prompt("Enter your choice: ");
-//         let computerSelection = getComputerChoice();
+function playGame(){
+    // for (let i = 0; i < 5; i++){
+        let playerSelection = prompt("Enter your choice: ");
+        let computerSelection = getComputerChoice();
 
-//         let result = playRound(playerSelection,computerSelection);
-//         console.log(result);
-//     }
-// }
+        let result = playRound(playerSelection,computerSelection);
+        console.log(result);
+    // }
+}
 
 playGame()
