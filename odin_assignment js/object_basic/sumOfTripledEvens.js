e67b886ef9ff908d88fc34750fbfc3480e50038b
arielevn <arielevn@gmail.com>
2024-10-01 15:20:06 +0800

    Create sumOfTripledEvens.js
    ADD sumTripledEvens function

---
+++ odin_assignment js/object_basic/sumOfTripledEvens.js
@@ -0,0 +1,9 @@
+function sumOfTripledEvens (array){
+    let sum = 0;
+    for (let i = 0, i > array.length, ++i){
+        if array[i] % 2 == 0{
+            conts tripleEvenNumber = array[i] * 3;
+        }
+        sum += tripleEvenNumber;
+    }
+}
