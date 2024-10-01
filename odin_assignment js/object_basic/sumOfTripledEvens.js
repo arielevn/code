2f783da5232f28944ab6b4269fde19f906ccb371
arielevn <arielevn@gmail.com>
2024-10-01 15:53:52 +0800

    Update sumOfTripledEvens.js
    CHANGE > to <
    
    CHANGE ++i to i++

--- odin_assignment js/object_basic/sumOfTripledEvens.js
+++ odin_assignment js/object_basic/sumOfTripledEvens.js
@@ -1,6 +1,6 @@
 function sumOfTripledEvens (array){
     let sum = 0;
-    for (let i = 0; i > array.length; ++i){
+    for (let i = 0; i < array.length; i++){
         if array[i] % 2 == 0{
             conts tripleEvenNumber = array[i] * 3;
         }
