3492f2aeda67bc700b444c571b4cd0a699405753
arielevn <arielevn@gmail.com>
2024-10-01 19:38:24 +0800

    Update sumOfTripledEvens.js
    ADD curves in IF for even number checker

--- odin_assignment js/object_basic/sumOfTripledEvens.js
+++ odin_assignment js/object_basic/sumOfTripledEvens.js
@@ -1,7 +1,7 @@
 function sumOfTripledEvens (array){
     let sum = 0;
     for (let i = 0; i < array.length; i++){
-        if array[i] % 2 == 0{
+        if (array[i] % 2 == 0){
             conts tripleEvenNumber = array[i] * 3;
         }
         sum += tripleEvenNumber;
