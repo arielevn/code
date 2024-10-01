4fd0b1ea87d83d5bf8d64a66670dbf3bf0215fc9
arielevn <arielevn@gmail.com>
2024-10-01 19:49:14 +0800

    Update sumOfTripledEvens.js
    ADD comments

--- odin_assignment js/object_basic/sumOfTripledEvens.js
+++ odin_assignment js/object_basic/sumOfTripledEvens.js
@@ -1,9 +1,12 @@
 function sumOfTripledEvens (array){
     let sum = 0;
+    // If the element is an even number
     for (let i = 0; i < array.length; i++){
+        // Multiply this number by three
         if (array[i] % 2 == 0){
             conts tripleEvenNumber = array[i] * 3;
         }
+        // Add the new number to the total
         sum += tripleEvenNumber;
     }
 
