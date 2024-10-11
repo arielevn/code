7ba907331480a7279bc437c35d3b09a0c382a877
arielevn <arielevn@gmail.com>
2024-10-11 20:48:59 +0800

    Update sumOfTripledEvens1.js

--- odin_assignment js/object_basic/sumOfTripledEvens1.js
+++ odin_assignment js/object_basic/sumOfTripledEvens1.js
@@ -1,11 +1,18 @@
 function sumOfTripledEvens(array) {
     return array
     .filter((num) => num % 2 === 0)
+      //checking if num is even
     .map((num) => num * 3)
+      // multiple num to 3
     .reduce((acc, curr) => acc + curr);
+      //sum of accumulated and currentItem
   }
 
 const arr = [1,2,3,4];
 const TripledEvens = sumOfTripledEvens(arr);
 
 console.log(TripledEvens);
+
+//question
+// acc = accumulated, where is it from?
+// curr = currentItem, where is it from?
