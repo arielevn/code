841d72b24f757113fb2ac577b86331b7298554f4
arielevn <arielevn@gmail.com>
2024-10-28 20:50:20 +0800

    Update script.js
    ADD if click matches the button

--- odin_assignment js/Calculator/script.js
+++ odin_assignment js/Calculator/script.js
@@ -1,4 +1,8 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator-keys')
 
-keys.addEventListener('Click', e => )
+keys.addEventListener('Click', e => {
+    if (e.target.matches('button')){
+        //do something
+    }
+})
