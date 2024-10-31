356ff307bf1d77dd7b04fbd6751c5f49cc236f30
arielevn <arielevn@gmail.com>
2024-10-31 21:18:01 +0800

    Update script.js
    MOVE comments

--- odin_assignment js/Calculator/script.js
+++ odin_assignment js/Calculator/script.js
@@ -2,9 +2,10 @@ const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator-keys')
 
 keys.addEventListener('click', e => {
+    if (e.target.matches('button')){
     //checks if the clicked element (e.target) is a button element within keys. 
     //This helps ensure only button clicks are processed (ignoring clicks outside buttons).
-    if (e.target.matches('button')){
+    
         //determine which type of key that is clicked
         const key = e.target
         const action = key.dataset.action
