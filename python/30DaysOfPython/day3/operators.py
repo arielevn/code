6bf43a4439edd25450458b56d3807337d3d5f164
arielevn <arielevn@gmail.com>
2026-03-30 16:57:08 +0800

    Create operators.py

---
+++ python/30DaysOfPython/day3/operators.py
@@ -0,0 +1,15 @@
+age = 29
+height = 175.5
+print('age: ' + str(age) + ' var type: ' + str(type(age)) )
+print('age: ' + str(height) + ' var type: ' + str(type(height)) )
+
+base = int(input('base: '))
+height = int(input('height: '))
+area_of_triagle = base * height / 2
+print(area_of_triagle)
+
+side_a = int(input('side a:'))
+side_b = int(input('side b:'))
+side_c = int(input('side c:'))
+perimeter_of_triangle = side_a + side_b + side_c
+print(perimeter_of_triangle) 
