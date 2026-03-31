8d363e687ab2178bee455e0ffe4451900d8aa990
arielevn <arielevn@gmail.com>
2026-03-31 23:18:35 +0800

    Update euclidean.py

--- python/30DaysOfPython/day3/euclidean.py
+++ python/30DaysOfPython/day3/euclidean.py
@@ -5,8 +5,5 @@ x2 = int(input('input x2: '))
 y1 = int(input('input y1: '))
 y2 = int(input('input y2: '))
 
-if x2-x1 == 0:
-    print('Euclidean Slope is undefined!')
-else:
-    E_Slope = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) 
-    print(E_Slope)
+distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) 
+print(f'euclidean distance is: {distance}')
