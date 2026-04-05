89fb643f77f49577e2965b826e725a2e4a6e3656
arielevn <arielevn@gmail.com>
2026-04-05 22:22:23 +0800

    Update length.py

--- python/30DaysOfPython/day3/length.py
+++ python/30DaysOfPython/day3/length.py
@@ -1,5 +1,9 @@
 x = str('python')
 print(f'the length of python is: {len(x)}')
 
-x = str('dragon')
-print(f'the length of dragon is: {len(x)}')
+y = str('dragon')
+print(f'the length of dragon is: {len(y)}')
+
+z = 'on'
+print(z in x and z in y)
+    
