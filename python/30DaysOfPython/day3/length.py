182ce61d65e400e5e815d87bc01c0e30469e4cc8
arielevn <arielevn@gmail.com>
2026-04-05 22:50:44 +0800

    Update length.py
    in
    not in
    if else

--- python/30DaysOfPython/day3/length.py
+++ python/30DaysOfPython/day3/length.py
@@ -7,3 +7,15 @@ print(f'the length of dragon is: {len(y)}')
 z = 'on'
 print(z in x and z in y)
     
+wish = 'I hope this course is not full of jargon'
+jargon = 'jargon'
+
+print(jargon in wish)
+
+print('no "on" in dragon and python: ' , z not in x and z not in y)
+
+num = int(input('input even number: '))
+if num % 2 == 0:
+    print('this is even number')
+else:
+    print('this is not an even number')
